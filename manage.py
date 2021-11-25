21934d796d7f535f724f6fa5191955fa52c9a007
NikitaSpirin321234 <nikitanikita04062000@gmail.com>
2021-11-25 21:34:13 +0300

    Update compose file

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from neomodel import config
 from web_app.app import create_app
 from web_app.db import connect
 
-config.DATABASE_URL = 'bolt://neo4j:testpass@localhost:11003'
+config.DATABASE_URL = 'bolt://neo4j:testpass@neo4j:7687'
 
 app = create_app()
 app.app_context().push()
